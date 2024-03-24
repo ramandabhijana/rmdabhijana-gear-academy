d6f4af0386bd293a5975a92ba6fe03e5881a1c96
Abhijana <abhijanaramanda@gmail.com>
2024-03-24 10:20:27 +0800

    chore: project setup

---
+++ io/src/lib.rs
@@ -0,0 +1,60 @@
+use gmeta::{In, InOut, Metadata, Out};
+use gstd::{Decode, Encode, TypeInfo};
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub struct PebblesInit {
+    pub difficulty: DifficultyLevel,
+    pub pebbles_count: u32,
+    pub max_pebbles_per_turn: u32,
+}
+
+#[derive(Debug, Default, Clone, Encode, Decode, TypeInfo)]
+pub enum DifficultyLevel {
+    #[default]
+    Easy,
+    Hard,
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum PebblesAction {
+    Turn(u32),
+    GiveUp,
+    Restart {
+        difficulty: DifficultyLevel,
+        pebbles_count: u32,
+        max_pebbles_per_turn: u32,
+    },
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum Player {
+    User,
+    Program,
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub enum PebblesEvent {
+    CounterTurn(u32),
+    Won(Player),
+}
+
+#[derive(Debug, Clone, Encode, Decode, TypeInfo)]
+pub struct GameState {
+    pub pebbles_count: u32,
+    pub max_pebbles_per_turn: u32,
+    pub pebbles_remaining: u32,
+    pub difficulty: DifficultyLevel,
+    pub first_player: Player,
+    pub winner: Option<Player>,
+}
+
+pub struct PebblesMetadata;
+
+impl Metadata for PebblesMetadata {
+    type Init = In<PebblesInit>;
+    type Handle = InOut<PebblesAction, PebblesEvent>;
+    type State = Out<GameState>;
+    type Reply = ();
+    type Others = ();
+    type Signal = ();
+}
