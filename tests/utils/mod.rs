a980a26cfb6e1bae4df6a4416ae209210490f714
Abhijana <abhijanaramanda@gmail.com>
2024-03-31 10:28:06 +0800

    chore: setup test utils

---
+++ tests/utils/mod.rs
@@ -0,0 +1,37 @@
+use gtest::{Program, System};
+use pebbles_game_io::{DifficultyLevel, PebblesInit};
+
+pub const PEBBLES_GAME_ID: u64 = 2;
+pub const USER_1: u64 = 3;
+pub const USER_2: u64 = 4;
+
+pub fn init_system() -> System {
+    let system = System::new();
+    system.init_logger();
+    system
+}
+
+pub fn init_pebbles_game(
+    sys: &System,
+    from: u64,
+    difficulty: DifficultyLevel,
+    pebbles_count: Option<u32>,
+    max_pebbles_per_turn: Option<u32>,
+) -> Program<'_> {
+    let game = Program::current_with_id(sys, PEBBLES_GAME_ID);
+
+    assert!(
+        game.send(
+            from,
+            PebblesInit {
+                difficulty,
+                pebbles_count: pebbles_count.unwrap_or(15),
+                max_pebbles_per_turn: max_pebbles_per_turn.unwrap_or(5),
+            },
+        )
+        .main_failed()
+            == false
+    );
+
+    game
+}
